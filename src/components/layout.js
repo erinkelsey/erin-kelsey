e89a653ecec3818c4cd463d2f0d4dd313b962b10
Erin Kelsey <erinkelsey0@gmail.com>
2022-10-15 12:50:57 -0600

    Add more gatsby packages and basic styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,64 @@
-import * as React from "react"
+import React, { useState, useEffect } from "react"
 // import { Link } from "gatsby"
+
+import styled, { ThemeProvider } from "styled-components"
+
+import { GlobalStyle, theme } from "@styles"
 
 import logo from "../images/logo.png"
 
 // import { heading } from "./layout.module.css"
-import { pageWrapper, navBar, navBarLogo } from "../styles/layout.module.sass"
+// import { pageWrapper, navBar, navBarLogo } from "../styles/layout.module.sass"
 
-const Layout = ({ pageTitle, children }) => {
+const StyledContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`
+
+const Layout = ({ pageTitle, children, location }) => {
+  const isHome = location.pathname === "/"
+  const [isLoading, setIsLoading] = useState(isHome)
+
+  // Sets target="_blank" rel="noopener noreferrer" on external links
+  const handleExternalLinks = () => {
+    const allLinks = Array.from(document.querySelectorAll("a"))
+    if (allLinks.length > 0) {
+      allLinks.forEach((link) => {
+        if (link.host !== window.location.host) {
+          link.setAttribute("rel", "noopener noreferrer")
+          link.setAttribute("target", "_blank")
+        }
+      })
+    }
+  }
+
+  useEffect(() => {
+    if (isLoading) {
+      return
+    }
+
+    if (location.hash) {
+      const id = location.hash.substring(1) // location.hash without the '#'
+      setTimeout(() => {
+        const el = document.getElementById(id)
+        if (el) {
+          el.scrollIntoView()
+          el.focus()
+        }
+      }, 0)
+    }
+
+    handleExternalLinks()
+  }, [isLoading])
+
   return (
-    <div className={pageWrapper}>
-      <nav className={navBar}>
-        <img src={logo} alt="Logo" className={navBarLogo} />
-        {/* <ul className={navLinks}>
+    <div id="root">
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <nav>
+          <img src={logo} alt="Logo" />
+          {/* <ul className={navLinks}>
           <li className={navLinkItem}>
             <Link to="/" className={navLinkText}>
               Home
@@ -23,11 +70,12 @@ const Layout = ({ pageTitle, children }) => {
             </Link>
           </li>
         </ul> */}
-      </nav>
-      <main>
-        <h1>{pageTitle}</h1>
-        {children}
-      </main>
+        </nav>
+        <main>
+          <h1>{pageTitle}</h1>
+          {children}
+        </main>
+      </ThemeProvider>
     </div>
   )
 }
---
+++ src/config.js
@@ -0,0 +1,63 @@
+module.exports = {
+  email: "erin@erinkelsey.io",
+
+  socialMedia: [
+    {
+      name: "GitHub",
+      url: "https://github.com/erinkelsey",
+    },
+    {
+      name: "Instagram",
+      url: "https://www.instagram.com/erinkelsey_",
+    },
+    {
+      name: "DataCamp",
+      url: "https://app.datacamp.com/profile/erinkelsey",
+    },
+    {
+      name: "Udemy",
+      url: "https://www.udemy.com/user/erin-kelsey-2",
+    },
+  ],
+
+  navLinks: [
+    {
+      name: "About",
+      url: "/#about",
+    },
+    {
+      name: "Experience",
+      url: "/#experience",
+    },
+    {
+      name: "Projects",
+      url: "/#projects",
+    },
+    {
+      name: "Contact",
+      url: "/#contact",
+    },
+  ],
+
+  colors: {
+    green: "#64FFB5",
+    pink: "#F1A4FE",
+    darkPurple: "#14002D",
+  },
+
+  srConfig: (delay = 200, viewFactor = 0.25) => ({
+    origin: "bottom",
+    distance: "20px",
+    duration: 500,
+    delay,
+    rotate: { x: 0, y: 0, z: 0 },
+    opacity: 0,
+    scale: 1,
+    easing: "cubic-bezier(0.645, 0.045, 0.355, 1)",
+    mobile: true,
+    reset: false,
+    useDelay: "always",
+    viewFactor,
+    viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
+  }),
+}
